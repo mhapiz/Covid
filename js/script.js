0c705b7cebd31f6b0b050dac2ddd5e92aba6a9eb
mhapiz <mhapiz10@gmail.com>
2020-03-18 11:26:06 +0800

    YEAY

--- js/script.js
+++ js/script.js
@@ -1,42 +1,86 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   let worldConfirm = document.querySelector("#world-confirm");
   let worldRecover = document.querySelector("#world-recover");
   let indoConfirm = document.querySelector("#indo-confirm");
   let indoRecover = document.querySelector("#indo-recover");
+  let updateSpan = document.querySelector(".up");
 
+  function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  }
 
-  let api = `https://coronavirus-tracker-api.herokuapp.com/all`;
+  fetch("https://api.kawalcorona.com/")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      let idConfirm;
+      let idRecover;
+      let update;
+      data.forEach(element => {
+        if (element.attributes.OBJECTID == 64) {
+          idConfirm = element.attributes.Confirmed;
+          idRecover = element.attributes.Recovered;
+          lastUpdate = new Date(element.attributes.Last_Update);
+        }
+      });
+      var hariHari = [
+        "Minggu",
+        "Senin",
+        "Selasa",
+        "Rabu",
+        "Kamis",
+        "Jum'at",
+        "Sabtu"
+      ];
 
-  fetch(api).then(response => {
-    return response.json();
-  }).then(data => {
-    let confirmed = data.latest.confirmed;
-    let recovered = data.latest.recovered;
-    let idConfirmed = data.confirmed.locations;
-    let idConfirm;
-    let idRecovered = data.recovered.locations;
-    let idRecover;
+      var bulanBulan = [
+        "Januari",
+        "Februari",
+        "Maret",
+        "April",
+        "Mei",
+        "Juni",
+        "Juli",
+        "Agustus",
+        "September",
+        "Oktober",
+        "Nopember",
+        "Desember"
+      ];
 
-    idConfirmed.forEach(element => {
-      if (element.country_code === "ID") {
-        idConfirm = element.latest;
-      }
+      var month = bulanBulan[lastUpdate.getUTCMonth() + 1]; //months from 1-12
+      var day = hariHari[lastUpdate.getUTCDay() + 1];
+      var date = lastUpdate.getUTCDate();
+      var year = lastUpdate.getUTCFullYear();
+      var hour = lastUpdate.getHours();
+      var minute = lastUpdate.getMinutes();
+      var fullJam = hour + ":" + minute;
+      var waktu =
+        day + ", " + date + " " + month + " " + year + " Jam " + fullJam;
+      console.log(fullJam);
+      updateSpan.textContent = waktu;
+      indoConfirm.textContent = formatNumber(idConfirm);
+      indoRecover.textContent = formatNumber(idRecover);
     });
 
-    idRecovered.forEach(element => {
-      if (element.country_code === "ID") {
-        idRecover = element.latest;
-      }
+  fetch("https://api.kawalcorona.com/positif/")
+    .then(data => {
+      return data.json();
+    })
+    .then(data => {
+      let confirmed = data.value;
+      worldConfirm.textContent = formatNumber(confirmed);
     });
 
-    function formatNumber(num) {
-      return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-    }
+  //
 
-    worldConfirm.textContent = formatNumber(confirmed);
-    worldRecover.textContent = formatNumber(recovered);
-    indoConfirm.textContent = formatNumber(idConfirm);
-    indoRecover.textContent = formatNumber(idRecover);
-  });
-
+  fetch("https://api.kawalcorona.com/sembuh/")
+    .then(data => {
+      return data.json();
+    })
+    .then(data => {
+      let recovered = data.value;
+      worldRecover.textContent = formatNumber(recovered);
+    });
 });
