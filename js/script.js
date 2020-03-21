8a215a4808969de5f6f900649dbff2cf4a0a3266
mhapiz <mhapiz10@gmail.com>
2020-03-21 12:23:17 +0800

    Not final version

--- js/script.js
+++ js/script.js
@@ -10,62 +10,65 @@ function liveCorona() {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
   }
 
-  setInterval(function () {
-    fetch("https://api.kawalcorona.com/").then((result) => {
-      return result.json();
-    }).then(data => {
-      let idConfirm;
-      let idRecover;
-      data.forEach(element => {
-        if (element.attributes.Country_Region == "Indonesia") {
-          idConfirm = element.attributes.Confirmed;
-          idRecover = element.attributes.Recovered;
-          lastUpdate = new Date(element.attributes.Last_Update);
-        }
+  setInterval(function() {
+    fetch("https://api.kawalcorona.com/")
+      .then(result => {
+        return result.json();
+      })
+      .then(data => {
+        let idConfirm;
+        let idRecover;
+        data.forEach(element => {
+          if (element.attributes.Country_Region == "Indonesia") {
+            idDeath = element.attributes.Deaths;
+            idConfirm = element.attributes.Confirmed;
+            idRecover = element.attributes.Recovered;
+            lastUpdate = new Date(element.attributes.Last_Update);
+          }
+        });
+        var hariHari = [
+          "Minggu",
+          "Senin",
+          "Selasa",
+          "Rabu",
+          "Kamis",
+          "Jum'at",
+          "Sabtu"
+        ];
+
+        var bulanBulan = [
+          "Januari",
+          "Februari",
+          "Maret",
+          "April",
+          "Mei",
+          "Juni",
+          "Juli",
+          "Agustus",
+          "September",
+          "Oktober",
+          "Nopember",
+          "Desember"
+        ];
+
+        var month = bulanBulan[lastUpdate.getUTCMonth() + 1]; //months from 1-12
+        var day = hariHari[lastUpdate.getUTCDay()];
+        var date = lastUpdate.getUTCDate();
+        var year = lastUpdate.getUTCFullYear();
+        var hour = lastUpdate.getHours();
+        var minute = lastUpdate.getMinutes();
+        var fullJam = hour + ":" + minute;
+        var waktu =
+          day + ", " + date + " " + month + " " + year + " Jam " + fullJam;
+
+        var s;
+        s = (idRecover / idConfirm) * 100;
+
+        updateSpan.textContent = waktu;
+        indoConfirm.textContent = formatNumber(idConfirm);
+        indoRecover.textContent = formatNumber(idRecover);
+        indoPercent.textContent = Math.round(s);
       });
-      var hariHari = [
-        "Minggu",
-        "Senin",
-        "Selasa",
-        "Rabu",
-        "Kamis",
-        "Jum'at",
-        "Sabtu"
-      ];
-
-      var bulanBulan = [
-        "Januari",
-        "Februari",
-        "Maret",
-        "April",
-        "Mei",
-        "Juni",
-        "Juli",
-        "Agustus",
-        "September",
-        "Oktober",
-        "Nopember",
-        "Desember"
-      ];
-
-      var month = bulanBulan[lastUpdate.getUTCMonth() + 1]; //months from 1-12
-      var day = hariHari[lastUpdate.getUTCDay() + 1];
-      var date = lastUpdate.getUTCDate();
-      var year = lastUpdate.getUTCFullYear();
-      var hour = lastUpdate.getHours();
-      var minute = lastUpdate.getMinutes();
-      var fullJam = hour + ":" + minute;
-      var waktu =
-        day + ", " + date + " " + month + " " + year + " Jam " + fullJam;
-
-      var s;
-      s = idRecover / 100 * idConfirm
-
-      updateSpan.textContent = waktu;
-      indoConfirm.textContent = formatNumber(idConfirm);
-      indoRecover.textContent = formatNumber(idRecover);
-      indoPercent.textContent = s;
-    });
 
     fetch("https://api.kawalcorona.com/positif/")
       .then(data => {
@@ -75,7 +78,6 @@ function liveCorona() {
         confirmed = data.value;
         worldConfirm.textContent = formatNumber(confirmed);
       });
-
 
     fetch("https://api.kawalcorona.com/sembuh/")
       .then(data => {
@@ -89,6 +91,6 @@ function liveCorona() {
   }, 3000);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  liveCorona()
+document.addEventListener("DOMContentLoaded", () => {
+  liveCorona();
 });
