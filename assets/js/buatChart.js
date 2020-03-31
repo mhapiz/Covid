e447055ee068a64b6aeafc6a98d835afbe8d5727
mhapiz <mhapiz10@gmail.com>
2020-03-31 21:45:47 +0800

    final redesign, ready for merge

--- assets/js/buatChart.js
+++ assets/js/buatChart.js
@@ -1,7 +1,11 @@
 function buatChart() {
   let daerah = new Array();
   let positif = new Array();
-  let canvasChart = document.getElementById('myChart');
+  let sembuh = new Array();
+  let meni = new Array();
+  let canvasChart = document.getElementById("myChart");
+  let tDaerah = document.querySelector("#tDaerah");
+  let tJumlah = document.querySelector("#tJumlah");
 
   fetch("https://api.kawalcorona.com/indonesia/provinsi/")
     .then(res => {
@@ -11,17 +15,47 @@ function buatChart() {
       data.forEach(el => {
         daerah.push(el.attributes.Provinsi);
         positif.push(el.attributes.Kasus_Posi);
+        sembuh.push(el.attributes.Kasus_Semb);
+        meni.push(el.attributes.Kasus_Meni);
+
+        $("#dataTab").append(
+          "<tr>" +
+          "<td>" +
+          el.attributes.Provinsi +
+          "</td>" +
+          "<td>" +
+          el.attributes.Kasus_Posi +
+          "</td>" +
+          "<td>" +
+          el.attributes.Kasus_Semb +
+          "</td>" +
+          "<td>" +
+          el.attributes.Kasus_Meni +
+          "</td>" +
+          "</tr>"
+        );
       });
+
       new Chart(canvasChart, {
-        type: "bar",
+        type: "horizontalBar",
         data: {
           labels: daerah,
           datasets: [{
-            label: "Yang Positif",
-            backgroundColor: "#c45850",
-            data: positif,
-            borderWidth: 1
-          }]
+              label: "Positif",
+              backgroundColor: "#00a8cc",
+              data: positif,
+            },
+            {
+              label: "Sembuh",
+              backgroundColor: "#21bf73",
+              data: sembuh,
+            },
+            {
+              label: "Meninggal",
+              backgroundColor: "#f0134d",
+              data: meni,
+            }
+          ]
         },
         options: {
           legend: {
